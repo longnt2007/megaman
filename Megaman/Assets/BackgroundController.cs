1c096e9988fea680bec1d22f65cd245224ad47a0
aof2 <aof2.2016@gmail.com>
2020-03-31 08:00:17 +0700

    - Added project with sample scrolling background

---
+++ Megaman/Assets/BackgroundController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    Material material;
+    Vector2 offset;
+
+    public int xVel, yVel;
+
+    private void Awake()
+    {
+        material = GetComponent<Renderer>().material;    
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = new Vector2(xVel,yVel);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        material.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
